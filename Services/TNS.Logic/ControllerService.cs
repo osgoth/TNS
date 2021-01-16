9fbdfd8fb444bddc903ecdec52b2c00da76d4ba5
dmitri <osgoth@protonmail.com>
2021-01-17 00:14:57 +0100

    Users call other users

---
+++ Services/TNS.Logic/ControllerService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TNS.Domain;
+using TNS.Database;
+
+namespace TNS.Logic
+{
+    public class ControllerService
+    {
+        // base station statuses:
+        // 0 - stopped
+        // 1 - running
+
+        // base station id to current capacity relation
+        public Dictionary<int, int> baseStationCapacity = new Dictionary<int, int>();
+
+        public ControllerService()
+        {
+            using (var dbContext = new TelecommunicationNetworkSimulatorContext())
+            {
+                for (int i = 0; i < dbContext.BaseStations.Count(); i++)
+                {
+                    baseStationCapacity.Add(i, 0);
+                }
+            }
+        }
+        public void RestartBaseStation(BaseStation baseStation)
+        {
+            using (var dbContext = new TelecommunicationNetworkSimulatorContext())
+            {
+                baseStation.BaseStationStatusId = 0;
+                foreach (var user in dbContext.Users.Where(i => i.BaseStationId == baseStation.Id))
+                {
+                    user.UserStatusId = 0;
+
+                }
+            }
+        }
+    }
+}
---
+++ Services/TNS.Logic/UserLogic.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using TNS.Domain;
+using TNS.Database;
+
+namespace TNS.Logic
+{
+    public static class UserLogic
+    {
+        // user statuses:
+        // 0 - free
+        // 1 - talking
+        // 2 - in queue
+
+        public static void StartChanceCalculation(this User user, ControllerService controllerService)
+        {
+            int chance = 30;    // in percents
+            int callDelay = 60; // in seconds
+
+            using (var dbContext = new TelecommunicationNetworkSimulatorContext())
+            {
+                var rnd = new Random();
+
+                while (true)
+                {
+                    int chanceGenerated = rnd.Next(100);
+                    if (
+                        dbContext.BaseStations
+                        .Where(bs => bs.Id == user.BaseStationId)
+                        .FirstOrDefault()
+                        .BaseStationStatusId == 1
+                        )
+                    {
+                        if (chanceGenerated > chance)
+                        {
+                            Thread.Sleep(callDelay * 1000);
+                        }
+                        else
+                        {
+                            User otherUser = dbContext.Users.ToList()[rnd.Next(dbContext.Users.Count())];
+                            while (otherUser == user)
+                            {
+                                otherUser = dbContext.Users.ToList()[rnd.Next(dbContext.Users.Count())];
+                            }
+                            user.Call(otherUser, controllerService);
+                        }
+                    }
+                }
+            }
+        }
+
+        public static void Call(this User user, User otherUser, ControllerService controllerService)
+        {
+            var rnd = new Random();
+
+            lock (otherUser)
+            {
+                if (otherUser.UserStatusId != 0)
+                {
+                    return;
+                }
+
+                controllerService.baseStationCapacity[user.BaseStationId]++;
+                controllerService.baseStationCapacity[otherUser.BaseStationId]++;
+                user.UserStatusId = 1;
+                otherUser.UserStatusId = 1;
+
+                Console.WriteLine($"++ {user.Number} -> {otherUser.Number}");
+                Thread.Sleep(rnd.Next(1000, 3000));
+                Console.WriteLine($"-- {user.Number} -> {otherUser.Number}");
+
+                controllerService.baseStationCapacity[user.BaseStationId]--;
+                controllerService.baseStationCapacity[otherUser.BaseStationId]--;
+                user.UserStatusId = 0;
+                otherUser.UserStatusId = 0;
+            }
+
+        }
+    }
+}
