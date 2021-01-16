a864eda4a574e2991e0a1d96ef15d5609c83a412
dmitri <osgoth@protonmail.com>
2021-01-16 20:00:54 +0100

    domain classes
    UserStatus, BaseStationStatus deleted from cs

---
+++ Services/TNS.Domain/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TNS.Domain
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public int BaseStationId { get; set; }
+        public int SubscriptionTypeId { get; set; }
+        public DateTime MinutesLeft { get; set; }
+        public string FullName { get; set; }
+        public int UserStatusId { get; set; }
+
+
+        public BaseStation BaseStation { get; set; }
+        public SubscriptionType SubscriptionType { get; set; }
+
+    }
+}
