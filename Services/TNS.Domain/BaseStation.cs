e92e2e547fbcc98e7f18627cd231230a79ac9fb3
osgoth <43686455+osgoth@users.noreply.github.com>
2021-01-16 20:11:53 +0100

    Domain classes creation (#2)
    * domain classes
    
    UserStatus, BaseStationStatus deleted from cs
    
    * UserStatus, BaseStationStatus deleted

---
+++ Services/TNS.Domain/BaseStation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TNS.Domain
+{
+    public class BaseStation
+    {
+        public int Id { get; set; }
+        public int Capacity { get; set; }
+        public int BaseStationStatusId { get; set; }
+
+        public int BaseStationStatus { get; set; }
+    }
+}
---
+++ Services/TNS.Domain/Constants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TNS.Domain
+{
+    public class Constants
+    {
+        public int Id { get; set; }
+        public int UserCallChance { get; set; }
+        public int UserTravelChance { get; set; }
+    }
+}
