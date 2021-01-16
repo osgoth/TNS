14204b47a63893e2105d5ac0c274696f953c261a
artem.vasylenko <vasylenko.w@gmail.com>
2021-01-16 22:11:49 +0200

    enable migrations and fill context with domain entities

--- Services/TNS.Database/TelecommunicationNetworkSimulatorContext.cs
+++ Services/TNS.Database/TelecommunicationNetworkSimulatorContext.cs
@@ -4,16 +4,21 @@ using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TNS.Domain;
 
 namespace TNS.Database
 {
     public class TelecommunicationNetworkSimulatorContext : DbContext
     {
         public TelecommunicationNetworkSimulatorContext() : base("DefaultConnection")
-        {
-            //public DbSet<Student> Students { get; set; }
+        { }
+        public DbSet<BaseStation> BaseStations { get; set; }
+        public DbSet<BaseStationLoad> BaseStationLoads { get; set; }
+        public DbSet<SubscriptionType> SubscriptionTypes { get; set; }
+        public DbSet<Constants> Constants { get; set; }
+        public DbSet<User> Users { get; set; }
 
-
-    }
     }
 }
+
+
