bf6d8eb12ae3851246089d6eeca203989d7be4bb
osgoth <43686455+osgoth@users.noreply.github.com>
2021-01-17 00:19:37 +0100

    Users call other users (#5)

--- Services/TNS.Engine/Program.cs
+++ Services/TNS.Engine/Program.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
+using TNS.Domain;
+using TNS.Logic;
+using TNS.Database;
 
 namespace TNS.Engine
 {
@@ -10,6 +14,28 @@ namespace TNS.Engine
     {
         static void Main(string[] args)
         {
+            ControllerService controllerService = new ControllerService();
+
+            using (var dbContext = new TelecommunicationNetworkSimulatorContext())
+            {
+                List<Thread> threads = new List<Thread>();
+                foreach (var user in dbContext.Users)
+                {
+                    threads.Add(
+                        new Thread(
+                            new ThreadStart(
+                                () => user.StartChanceCalculation(controllerService)
+                                )
+                            )
+                        );
+                }
+
+                foreach (var thr in threads)
+                {
+                    thr.Start();
+                }
+            }
+
         }
     }
 }
