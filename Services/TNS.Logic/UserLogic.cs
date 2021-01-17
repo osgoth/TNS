9d3cec8cbc880fdd9e0eec649bc168ae7742d409
dmitri <osgoth@protonmail.com>
2021-01-17 04:01:32 +0100

    calls work with db
    all calls are unique, nothing blocks forever

--- Services/TNS.Logic/UserLogic.cs
+++ Services/TNS.Logic/UserLogic.cs
@@ -12,44 +12,40 @@ namespace TNS.Logic
     public static class UserLogic
     {
         // user statuses:
-        // 0 - free
-        // 1 - talking
-        // 2 - in queue
+        // 1 - free
+        // 2 - talking
+        // 3 - in queue # not realised yet
 
-        public static void StartChanceCalculation(this User user, ControllerService controllerService)
+        public static void StartChanceCalculation(this User user, List<User> users, ControllerService controllerService)
         {
-            int chance = 30;    // in percents
-            int callDelay = 60; // in seconds
-
+            int callChance;    // in percents
+            int callDelay; // in seconds
             using (var dbContext = new TelecommunicationNetworkSimulatorContext())
             {
-                var rnd = new Random();
+                callChance = dbContext.Constants.FirstOrDefault().UserCallChance;
+                callDelay = dbContext.Constants.FirstOrDefault().CallDelay;
+            }
 
-                while (true)
+            var rnd = new Random();
+
+            while (true)
+            {
+                int chanceGenerated = rnd.Next(100);
+
+                if (chanceGenerated > callChance)
                 {
-                    int chanceGenerated = rnd.Next(100);
-                    if (
-                        dbContext.BaseStations
-                        .Where(bs => bs.Id == user.BaseStationId)
-                        .FirstOrDefault()
-                        .BaseStationStatusId == 1
-                        )
-                    {
-                        if (chanceGenerated > chance)
-                        {
-                            Thread.Sleep(callDelay * 1000);
-                        }
-                        else
-                        {
-                            User otherUser = dbContext.Users.ToList()[rnd.Next(dbContext.Users.Count())];
-                            while (otherUser == user)
-                            {
-                                otherUser = dbContext.Users.ToList()[rnd.Next(dbContext.Users.Count())];
-                            }
-                            user.Call(otherUser, controllerService);
-                        }
-                    }
+                    Thread.Sleep(callDelay * 1000);
                 }
+                else
+                {
+                    User otherUser = users.ToList()[rnd.Next(users.Count())];
+                    while (otherUser == user && otherUser.UserStatusId != 1)
+                    {
+                        otherUser = users.ToList()[rnd.Next(users.Count())];
+                    }
+                    user.Call(otherUser, controllerService);
+                }
+
             }
         }
 
@@ -57,27 +53,36 @@ namespace TNS.Logic
         {
             var rnd = new Random();
 
-            lock (otherUser)
+            Semaphore smp = new Semaphore(1, 1);
+
+            if (otherUser.UserStatusId != 1)
             {
-                if (otherUser.UserStatusId != 0)
-                {
-                    return;
-                }
-
-                controllerService.baseStationCapacity[user.BaseStationId]++;
-                controllerService.baseStationCapacity[otherUser.BaseStationId]++;
-                user.UserStatusId = 1;
-                otherUser.UserStatusId = 1;
-
-                Console.WriteLine($"++ {user.Number} -> {otherUser.Number}");
-                Thread.Sleep(rnd.Next(1000, 3000));
-                Console.WriteLine($"-- {user.Number} -> {otherUser.Number}");
-
-                controllerService.baseStationCapacity[user.BaseStationId]--;
-                controllerService.baseStationCapacity[otherUser.BaseStationId]--;
-                user.UserStatusId = 0;
-                otherUser.UserStatusId = 0;
+                return;
             }
+
+            smp.WaitOne();
+            Monitor.Enter(otherUser);
+            controllerService.baseStationCapacity[user.BaseStationId]++;
+            controllerService.baseStationCapacity[otherUser.BaseStationId]++;
+            user.UserStatusId = 2;
+            otherUser.UserStatusId = 2;
+            Monitor.Exit(otherUser);
+            smp.Release();
+
+            Console.WriteLine($"++ {user.Number} -> {otherUser.Number}");
+            Thread.Sleep(rnd.Next(1000, 30000));
+            Console.WriteLine($"-- {user.Number} -> {otherUser.Number}");
+
+
+            smp.WaitOne();
+            Monitor.Enter(otherUser);
+            controllerService.baseStationCapacity[user.BaseStationId]--;
+            controllerService.baseStationCapacity[otherUser.BaseStationId]--;
+            user.UserStatusId = 1;
+            otherUser.UserStatusId = 1;
+            Monitor.Exit(otherUser);
+            smp.Release();
+
 
         }
     }
