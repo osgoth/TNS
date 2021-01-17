b31b6dff73d36cce4b738c14d8edebaa08a20bb8
artem.vasylenko <vasylenko.w@gmail.com>
2021-01-17 02:55:21 +0200

    add BaseStationRepository, UserRepository, extend EF context

---
+++ Services/TNS.Domain/User/IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TNS.Domain
+{
+    public interface IUserRepository
+    {
+        Task<List<User>> GetUsers();
+
+        Task<User> GetUserByIdAsync(int id);
+
+    }
+}
